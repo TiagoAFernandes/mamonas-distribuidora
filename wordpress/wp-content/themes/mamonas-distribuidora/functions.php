20e539814f9f38c7281bd97aee77440877169695
TiagoAFernandes <tiago.fernandes@diwe.com.br>
2025-04-01 19:58:57 -0300

    feat(theme): otimiza carregamento de scripts no header
    - Adiciona enfileiramento correto de CSS e JS no functions.php
    - Remove links diretos e usa wp_enqueue_scripts
    - Melhora suporte a menus no WordPress

--- wordpress/wp-content/themes/mamonas-distribuidora/functions.php
+++ wordpress/wp-content/themes/mamonas-distribuidora/functions.php
@@ -2,6 +2,34 @@
 
 require_once("functions/custom-registers.php");
 
+function meu_tema_recursos()
+{
+    add_theme_support('title-tag'); // Suporte para título dinâmico
+    register_nav_menus([
+        'main-menu' => __('Menu Principal', 'meu-tema')
+    ]);
+}
+add_action('after_setup_theme', 'meu_tema_recursos');
+
+function meu_tema_scripts()
+{
+    // CSS Local
+    // wp_enqueue_style('theme-fonts', get_template_directory_uri() . '/src/fonts/stylesheet.css', [], null);
+    wp_enqueue_style('theme-reset', get_template_directory_uri() . '/src/css/reset.css', [], null);
+    wp_enqueue_style('theme-style', get_template_directory_uri() . '/src/css/style.css', [], null);
+
+    // Bibliotecas Externas
+    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css', [], '6.1.1');
+    wp_enqueue_style('slick-theme', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css', [], '1.8.1');
+    wp_enqueue_style('slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css', [], '1.8.1');
+
+    // JS Externo
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('slick-js', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', ['jquery'], '1.8.1', true);
+}
+add_action('wp_enqueue_scripts', 'meu_tema_scripts');
+
+
 add_action('wp_head', 'myplugin_ajaxurl');
 
 add_action('after_setup_theme', 'theme_setup');
